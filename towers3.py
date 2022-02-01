d79eb6675bd69baa51b3b4ed6311a86ae40fc4ab
tai_falor <tai@penguin>
2022-02-01 15:56:37 -0700

    need to make level() work...

---
+++ towers3.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Tower3(pygame.sprite.Sprite):
+    def __init__(self):
+        self.x = 1000
+        self.y = 1000
+        self.speed = 1
+        self.range = 150
+        self.dmg = 1
